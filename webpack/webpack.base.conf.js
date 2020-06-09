3b504b444ba38109a570dbd6df2da1b94a789d88
kyzinatra <danilsolom@mail.ru>
2020-06-09 18:57:21 +1000

    add plugin-proposal-private-methods

--- webpack/webpack.base.conf.js
+++ webpack/webpack.base.conf.js
@@ -65,6 +65,7 @@ module.exports = {
           plugins: [
             "@babel/plugin-proposal-object-rest-spread",
             "@babel/plugin-proposal-class-properties",
+            "@babel/plugin-proposal-private-methods",
           ],
         },
         exclude: /node_modules/,
