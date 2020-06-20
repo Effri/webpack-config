b5564c0dadbee687d78ca0ce96e6a257098fd586
kyzinatra <danilsolom@mail.ru>
2020-06-20 20:48:13 +1000

    Add hooks

--- src/components/Comments/CommentBox.jsx
+++ src/components/Comments/CommentBox.jsx
@@ -1,55 +1,37 @@
 /** @format */
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Comment from "./Comment";
 import Logo from "./Logo";
 import CreateComment from "./CreateComment";
 import { themes, ThemeContext } from "../../js/theme-context";
 
-class CommentBox extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      theme: themes.dark,
-      comments: this.props.comments,
-    };
-    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
-  }
+function CommentBox(props) {
+  const [comments, setComments] = useState(props.comments);
+  const handleCommentSubmit = (c) => setComments(comments.concat([c]));
 
-  handleCommentSubmit(comment) {
-    const comments = this.state.comments;
-    const newComments = comments.concat([comment]);
-    this.setState({
-      comments: newComments,
-    });
-  }
-
-  render() {
-    return (
-      <div className="comments container">
-        <div className="comments__logo">
-          <Logo title={this.props.title}></Logo>
-        </div>
-        <ThemeContext.Provider value={this.state.theme}>
-          <div className="comments__items">
-            {this.state.comments.map((comment) => {
-              return (
-                <Comment
-                  key={comment.id}
-                  id={comment.id}
-                  user={comment.user}
-                  content={comment.content}
-                  img={comment.img}
-                ></Comment>
-              );
-            })}
-          </div>
-        </ThemeContext.Provider>
-        <CreateComment
-          onCommentSubmit={this.handleCommentSubmit}
-        ></CreateComment>
+  return (
+    <div className="comments container">
+      <div className="comments__logo">
+        <Logo title={props.title}></Logo>
       </div>
-    );
-  }
+      <ThemeContext.Provider value={themes.dark}>
+        <div className="comments__items">
+          {comments.map((comment) => {
+            return (
+              <Comment
+                key={comment.id}
+                id={comment.id}
+                user={comment.user}
+                content={comment.content}
+                img={comment.img}
+              ></Comment>
+            );
+          })}
+        </div>
+      </ThemeContext.Provider>
+      <CreateComment onCommentSubmit={handleCommentSubmit}></CreateComment>
+    </div>
+  );
 }
 
 export default CommentBox;
--- src/components/Comments/CreateComment.jsx
+++ src/components/Comments/CreateComment.jsx
@@ -1,63 +1,45 @@
 /** @format */
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-class CreateComment extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      content: "",
-      user: "",
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleUserChange = this.handleUserChange.bind(this);
-    this.handleTextChange = this.handleTextChange.bind(this);
-  }
-  handleSubmit(e) {
+function CreateComment(props) {
+  const [content, setContent] = useState("");
+  const [user, setUser] = useState("");
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.props.onCommentSubmit({
-      user: this.state.user,
-      content: this.state.content,
+    props.onCommentSubmit({
+      user: user,
+      content: content,
       id: Math.random() * 20,
     });
-    this.setState(() => ({
-      user: "",
-      content: "",
-    }));
-  }
+    setContent("");
+    setUser("");
+  };
+  const handleUserChange = (e) => {
+    const text = e.target.value;
+    setUser(text);
+  };
+  const handleTextChange = (e) => {
+    const text = e.target.value;
+    setContent(text);
+  };
 
-  handleUserChange(event) {
-    const text = event.target.value;
-    this.setState(() => ({
-      user: text,
-    }));
-  }
-
-  handleTextChange(event) {
-    const text = event.target.value;
-    this.setState(() => ({
-      content: text,
-    }));
-  }
-
-  render() {
-    return (
-      <form className="comments__new" onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          placeholder="Your name"
-          value={this.state.user}
-          onChange={this.handleUserChange}
-        />
-        <textarea
-          value={this.state.content}
-          placeholder="Your comment"
-          onChange={this.handleTextChange}
-        ></textarea>
-        <button type="submit">Отправить</button>
-      </form>
-    );
-  }
+  return (
+    <form className="comments__new" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Your name"
+        value={user}
+        onChange={handleUserChange}
+      />
+      <textarea
+        value={content}
+        placeholder="Your comment"
+        onChange={handleTextChange}
+      ></textarea>
+      <button type="submit">Отправить</button>
+    </form>
+  );
 }
 
 export default CreateComment;
