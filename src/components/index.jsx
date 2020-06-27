df9cecf796beaa84b36b93a61eb577e4f8cd534b
kyzinatra <danilsolom@mail.ru>
2020-06-27 12:35:04 +1000

    add redux devtools

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,12 @@
 /** @format */
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM, { render } from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { reducers } from "../js/redusers";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+
+render(<Provider store={store}></Provider>, document.getElementById("root"));
