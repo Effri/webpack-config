dc0478c2e85b7a9958d42a9f3032b49b286f37c6
kyzinatra <danilsolom@mail.ru>
2020-06-30 19:58:36 +1000

    Add router server

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,12 +1,21 @@
 /** @format */
 import React from "react";
-import ReactDOM, { render } from "react-dom";
+import { render } from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { reducers } from "../js/redusers";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import { Route } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
 
-render(<Provider store={store}></Provider>, document.getElementById("root"));
+render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root"),
+);
